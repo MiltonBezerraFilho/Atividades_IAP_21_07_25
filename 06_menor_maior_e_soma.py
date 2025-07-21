65b5977a329b21d09490b16280ee2b569597a24b
Milton Bezerra Filho <miltonbfilho206@gmail.com>
2025-07-21 12:03:36 -0300

    Add files via upload

---
+++ 06_menor_maior_e_soma.py
@@ -0,0 +1,10 @@
+INCOMPLETO
+# Faça um programa que leia números repetidamente até que o valor -1 seja digitado. 
+# Quando isso ocorrer, o programa deve exibir o menor valor, o maior valor e a soma dos valores.
+
+soma = 0
+menor = 0
+maior = 999999999999999
+
+while True:
+    entrada = float( input("Digite um número: "))
