cf59097d541d10d58b23471a968ca79a2b7f93fc
Milton Bezerra Filho <miltonbfilho206@gmail.com>
2025-07-25 20:45:58 -0300

    Update 04_determinador_algorismos_numero_inteiro.py

--- 04_determinador_algorismos_numero_inteiro.py
+++ 04_determinador_algorismos_numero_inteiro.py
@@ -1,29 +1,12 @@
-INCOMPLETO
 # Escreva um programa para determinar o número de algarismos de um número inteiro positivo dado.
 
-numero = ( input("Insira um número inteiro: "))
-print(f'O número de algarismos é:{len(numero)}')
-
-
-
-
-
-
-
-
-
-
-'''
+numero = int( input("Insira um número inteiro e positivo: "))
 algarismos = 1
 
-if (numero >= 1):
-    divisao = numero / 10
-    if (divisao >= 1):
+if (numero > -1 and numero < 10):
+    print(f'O número de algarismos é {algarismos}.')
+else:
+    while (numero / 10 >= 1):
         algarismos += 1
-        nova_divisao = divisao / 10
-        while (nova_divisao / 10 != 0):
-            nova_divisao / 10
-            algarismos += 1
-
-print(f'O número de algarismos é {algarismos}.')
-'''
+        numero = numero / 10
+    print(f'O número de algarismos é {algarismos}.')
